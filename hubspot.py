f9074a8e9d1691a2b5b0d53ec12e2f9bd8c06362
Kevin Nasto <kevinnasto@gmail.com>
2018-07-18 20:17:07 -0500

    Updated argument to include token

--- hubspot.py
+++ hubspot.py
@@ -10,8 +10,8 @@ API_BASE_URL = 'https://api.hubapi.com'
 KEY_FILENAME = 'db_keys - Sheet1.csv'
 
 
-def get_hubspot_data(resource):
-    '''Given the resource URL returns a Python dictionary from the JSON'''
+def get_hubspot_data(resource, oath_token):
+    '''Given the Hubspot resource URL and token returns a Python dictionary from the JSON'''
     resource.strip('/')
     with urlopen(API_BASE_URL + resource + '?hapikey=' + oath_token) as response:
         data = json.load(response)
@@ -24,7 +24,8 @@ with open(KEY_FILENAME) as csvfile:
     for row in reader:
     	oath_token = row['Hubspot']
 
-contacts = get_hubspot_data('/contacts/v1/lists/all/contacts/all/')
+contacts_resource = '/contacts/v1/lists/all/contacts/all/'
+contacts = get_hubspot_data(contacts_resource, oath_token)
 
 # Get a particular contact ID from contacts
 for contact in contacts['contacts']:
@@ -33,7 +34,8 @@ for contact in contacts['contacts']:
         contact_id = contact['vid']
 
 # Get contact info for a particular contact ID
-contact_info = get_hubspot_data('/contacts/v1/contact/vid/{}/profile'.format('2401'))
+contact_info_resource = '/contacts/v1/contact/vid/{}/profile'.format('2401')
+contact_info = get_hubspot_data(contact_info_resource, oath_token)
 pprint(contact_info)
 
 
